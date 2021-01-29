20bf823c2ed631dbb8e2a63903151b40c25603d4
Jordi <jorllorber@gmail.com>
2021-01-29 19:40:17 +0100

    Fondo y mov camara

---
+++ Assets/Scripts/MovimientoCamara.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoCamara : MonoBehaviour
+{
+    public GameObject jugador;
+
+    // 0 limite iquierda  // 1 limite derecha
+    public GameObject [] limites = new GameObject [2];
+    private void FixedUpdate()
+    {
+        transform.position = new Vector3(jugador.transform.position.x, 0, transform.position.z);
+        transform.position = new Vector3(0, jugador.transform.position.y, transform.position.z);
+
+        if (jugador.transform.position.x > limites[0].transform.position.x && jugador.transform.position.x < limites[1].transform.position.x)
+            transform.position = new Vector3(jugador.transform.position.x, 0, transform.position.z);
+        else if (jugador.transform.position.x < limites[0].transform.position.x)
+            transform.position = new Vector3(limites[0].transform.position.x, 0, transform.position.z);
+        else
+            transform.position = new Vector3(limites[1].transform.position.x, 0, transform.position.z);
+
+    }
+}
