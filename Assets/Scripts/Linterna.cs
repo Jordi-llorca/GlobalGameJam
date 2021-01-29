10ee84858a823106bc53c15f209ef70a5a520894
delniss <De12li13a14@gmail.com>
2021-01-29 22:12:04 +0100

    Linterna Nivel 3

---
+++ Assets/Scripts/Linterna.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEditor.Experimental.SceneManagement;
+public class Linterna : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject linterna;
+    private bool Enabled;
+    void Start()
+    {
+        Enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            if(Enabled)
+            {
+                linterna.SetActive(Enabled);
+                Enabled = false;
+            }else {
+                linterna.SetActive(Enabled);
+                Enabled = true;
+            }
+        }
+    }
+}
