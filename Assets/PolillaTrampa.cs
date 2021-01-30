d6229f24d22c0a22a3d676e7724fc792b8d148db
Jordi <jorllorber@gmail.com>
2021-01-30 17:26:14 +0100

    Enemigos

---
+++ Assets/PolillaTrampa.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PolillaTrampa : MonoBehaviour
+{
+    private PlayerPos player;
+    private GameObject p;
+    public float vel;
+    public bool iluminado;
+    public bool sound;
+
+    public Animator transition;
+    public int direccion;
+    void Start()
+    {
+        p = GameObject.FindGameObjectWithTag("Player");
+        player = p.GetComponent<PlayerPos>();
+        iluminado = false;
+        sound = true;
+    }
+
+    void Update()
+    {
+        direccion = transform.position.x < p.transform.position.x ? 1 : -1;
+        if (iluminado)
+        {
+            transition.SetBool("Iluminado", true);
+            transform.Translate(Vector3.right * vel * direccion * Time.deltaTime);
+        } else {
+            transition.SetBool("Iluminado", false);
+        }
+        iluminado = false;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            player.muerte = true;
+        }
+    }
+}
