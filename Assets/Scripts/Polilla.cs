2718cf1dd0d3afbadd3d6e76c57a16b3d4a36b93
delniss <De12li13a14@gmail.com>
2021-01-30 14:06:16 +0100

    polilla 2

--- Assets/Scripts/Polilla.cs
+++ Assets/Scripts/Polilla.cs
@@ -13,7 +13,7 @@ public class Polilla : MonoBehaviour
     public float offsetX;
     public float offsetY;
 
-    public bool iluminado;
+    
 
     //perseguir al jugador
     public Transform jugador;
@@ -24,7 +24,6 @@ public class Polilla : MonoBehaviour
     private void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerPos>();
-        iluminado = false;
         sound = true;
         rb = GetComponent<Rigidbody2D>();
         localSpeed = moveSpeed;
@@ -32,13 +31,13 @@ public class Polilla : MonoBehaviour
     void FixedUpdate()
     {
         RaycastHit2D hit = Physics2D.Raycast(transform.position + new Vector3(offsetX, offsetY, 0) , Vector2.left, range, layerMask);
-        float distToPlayer = Vector2.Distance(transform.position, jugador.position);
-        if (hit.collider)
+        
+        if (hit.collider==null)
         {
             Debug.DrawRay(transform.position + new Vector3(offsetX, offsetY, 0), transform.TransformDirection(Vector3.left) * hit.distance, Color.yellow);
-            rb.constraints = RigidbodyConstraints2D.FreezePositionX;
             transition.SetTrigger("Pause");
-        } else {
+        } 
+        if (hit.collider){
             Debug.DrawRay(transform.position + new Vector3(offsetX, offsetY, 0), transform.TransformDirection(Vector3.left) * range, Color.white);
             if (sound) { FindObjectOfType<AudioManager>().Play("Antipolilla"); sound = false; }
             transition.SetTrigger("Ataque");
