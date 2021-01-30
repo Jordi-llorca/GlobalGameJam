2052dc84989da8f05b8164b763b9d0bf35a3d60a
delniss <De12li13a14@gmail.com>
2021-01-30 17:43:11 +0100

    pinchos

--- Assets/Scripts/PlayerPos.cs
+++ Assets/Scripts/PlayerPos.cs
@@ -6,7 +6,7 @@ public class PlayerPos : MonoBehaviour
 {
     private GameMaster gm;
     private LevelLoader ll;
-
+    private Rigidbody2D rb;
     public float to_die_time;
 
     public bool muerte;
@@ -14,7 +14,7 @@ public class PlayerPos : MonoBehaviour
         gm = GameObject.FindGameObjectWithTag("GM").GetComponent<GameMaster>();
         ll = GameObject.FindGameObjectWithTag("LL").GetComponent<LevelLoader>();
         transform.position = gm.lastCheckPointPos;
-
+        rb = GetComponent<Rigidbody2D>();
         muerte = false;
     }
     void Update()
@@ -26,6 +26,7 @@ public class PlayerPos : MonoBehaviour
 
     IEnumerator Muerte()
     {
+        rb.constraints = RigidbodyConstraints2D.FreezeAll;
         yield return new WaitForSeconds(to_die_time);
         ll.ReloadLevel();
     }
---
+++ Assets/Scripts/pinchos.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pinchos : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private PlayerPos player;
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerPos>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (collision.collider.tag=="Player")
+            {
+                player.muerte = true;
+            }
+        }
+}
